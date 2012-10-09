70092286de531f52e8fc2e1c945990581da51b43
Simon Stastny <stastny.simon@gmail.com>
2012-10-09 10:29:45 +0200

    asg3 - original sources

---
+++ 3-xtend/qa.xtext.tests/src-gen/examples/Example1.java
@@ -0,0 +1,72 @@
+package examples;
+public class Example1 implements Runnable {
+	
+	private tdt4250.io.AbstractIO io = new tdt4250.io.ConsoleIO();
+	
+	public void run() {
+		//Generated codes for a question
+		    {
+		        io.println("Here comes a question");
+		        //repeat the following until a correct answer is given, or hit the maximum tries if there is.
+		    	//1.print out the question,
+		        //2.get user input 
+		        //3.compare user input with the correct answer, and notify user correct or wrong answer
+		    }
+		
+		//Generated codes for a section
+		{
+		io.println("Here comes a section");
+		//Generated codes for a question
+		    {
+		        io.println("Here comes a question");
+		        //repeat the following until a correct answer is given, or hit the maximum tries if there is.
+		    	//1.print out the question,
+		        //2.get user input 
+		        //3.compare user input with the correct answer, and notify user correct or wrong answer
+		    }
+		
+		//Generated codes for a question
+		    {
+		        io.println("Here comes a question");
+		        //repeat the following until a correct answer is given, or hit the maximum tries if there is.
+		    	//1.print out the question,
+		        //2.get user input 
+		        //3.compare user input with the correct answer, and notify user correct or wrong answer
+		    }
+		
+		//Generated codes for a question
+		    {
+		        io.println("Here comes a question");
+		        //repeat the following until a correct answer is given, or hit the maximum tries if there is.
+		    	//1.print out the question,
+		        //2.get user input 
+		        //3.compare user input with the correct answer, and notify user correct or wrong answer
+		    }
+		}
+		//Generated codes for a section
+		{
+		io.println("Here comes a section");
+		//Generated codes for a question
+		    {
+		        io.println("Here comes a question");
+		        //repeat the following until a correct answer is given, or hit the maximum tries if there is.
+		    	//1.print out the question,
+		        //2.get user input 
+		        //3.compare user input with the correct answer, and notify user correct or wrong answer
+		    }
+		
+		//Generated codes for a question
+		    {
+		        io.println("Here comes a question");
+		        //repeat the following until a correct answer is given, or hit the maximum tries if there is.
+		    	//1.print out the question,
+		        //2.get user input 
+		        //3.compare user input with the correct answer, and notify user correct or wrong answer
+		    }
+		}
+	}
+
+	public static void main(String[] args) {
+		new Example1().run();
+	}
+}
